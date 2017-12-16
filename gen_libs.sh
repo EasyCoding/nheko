1ffced01ef6d9245af40c12de5596ec4a2abab7c
Vitaly Zaitsev <vitaly@easycoding.org>
2017-12-16 17:51:09 +0100

    Added shell script.

---
+++ gen_libs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo -n "Downloading header-only libraries..."
+mkdir deps
+wget https://github.com/nlohmann/json/releases/download/v2.1.1/json.hpp -O deps/json.hpp 2> /dev/null
+wget https://github.com/mpark/variant/releases/download/v1.3.0/variant.hpp -O deps/variant.hpp 2> /dev/null
+echo " Done."
+
+echo -n "Generating tarball..."
+tar czf header_only.tar.gz deps
+echo " Done."
