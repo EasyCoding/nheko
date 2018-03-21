0ee16789a4750e2cbdcbb6f58a3403a6a7f685ac
Vitaly Zaitsev <vitaly@easycoding.org>
2018-03-21 13:56:57 +0100

    Count hashes of header files, not generated tarball.

--- gen_libs.sh
+++ gen_libs.sh
@@ -8,14 +8,13 @@ wget https://github.com/mpark/variant/releases/download/v1.3.0/variant.hpp -O de
 echo " Done."
 
 echo -n "Generating tarball..."
+nf=header_only-$(cat deps/*.hpp | sha256sum | awk '{print substr ($1, 0, 7)}').tar.gz
 tar czf header_only.tar.gz deps
-nf=header_only-$(sha256sum header_only.tar.gz | awk '{print substr ($1, 0, 7)}').tar.gz
-echo " Done."
-
-echo -n "Renaming tarball to $nf..."
 mv header_only.tar.gz $nf
 echo " Done."
 
 echo -n "Removing temporary files..."
 rm -rf deps
 echo " Done."
+
+echo "Result: $nf"
